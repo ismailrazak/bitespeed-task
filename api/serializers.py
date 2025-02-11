9c0db912c27caf7a85e29e2ea5aa089af10ebb81
ismail <ismailrazak009@gmail.com>
2025-02-11 20:58:29 +0530

    Fix logic issues in contact serializer create method

--- api/serializers.py
+++ api/serializers.py
@@ -6,29 +6,38 @@ from api.models import Contact
 
 
 class ContactSerializer(serializers.ModelSerializer):
-
+    email = serializers.EmailField(allow_blank=True,allow_null=True)
+    phone_number = serializers.IntegerField(allow_null=True)
     class Meta:
         model = Contact
-        fields = ('customer', 'email', 'phone_number', 'linked_id', 'linked_precedence', 'created_at', 'updated_at', 'deleted_at', )
-
+        fields = ( 'email', 'phone_number', 'linked_id', 'linked_precedence', 'created_at', 'updated_at', 'deleted_at', )
+        read_only_fields = ( 'linked_id', 'linked_precedence', 'created_at', 'updated_at', 'deleted_at', )
 
     def create(self, validated_data):
 
             email=validated_data.get("email")
             phone_number =validated_data.get("phone_number")
             contact = Contact.objects.filter(email=email,phone_number=phone_number)
+            primary_contacts = Contact.objects.filter(Q(email=email)| Q(phone_number=phone_number),linked_precedence="primary").order_by("created_at")
             if contact:
                 return contact
-            primary_contacts = Contact.objects.filter(Q(email=email),Q(phone_number=phone_number),linked_precedence="primary").order_by("created_at")
-            if primary_contacts:
-                primary_contact=primary_contacts.pop(0)
-                for contact in primary_contacts:
+            elif primary_contacts:
+                print("primary_contacts",primary_contacts)
+                primary_contact=primary_contacts.first()
+                for contact in primary_contacts[1:]:
                     contact.linked_id =primary_contact.id
                     contact.linked_precedence='secondary'
                     contact.save()
-                secondary_contact=Contact.objects.create(linked_id = primary_contact.id,linked_precedence="secondary",**validated_data)
-                return secondary_contact
-            new_primary_contact=Contact.objects.create(linked_precedence='primary',**validated_data)
-            return new_primary_contact
+                if primary_contacts.count()==1:
+                    secondary_contact=Contact.objects.create(linked_id = primary_contact.id,linked_precedence="secondary",**validated_data)
+                    return secondary_contact
+                return primary_contact
+            else:
+                new_primary_contact=Contact.objects.create(linked_precedence='primary',**validated_data)
+                print("new_primary_contact",new_primary_contact)
+                return new_primary_contact
 
 
+# TODO : FIX NULL ALSO BEING ACCEPOTED
+# TODO FIX PHONE NUMBER DUPLCITE IN REPSOEN
+#TODO DIX PHONE NO TO STRING FIELDS
--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
+from django.db.models import Q
 from django.shortcuts import render
+from rest_framework.response import Response
+from  rest_framework.views import APIView
 
-# Create your views here.
+from api.models import Contact
+from api.serializers import ContactSerializer
+
+
+class IdentifyView(APIView):
+    def post(self,request,pk=None):
+        serializer = ContactSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        email=request.data.get('email')
+        phone_number =request.data.get('phone_number')
+        contacts=Contact.objects.filter(Q(email=email)|Q(phone_number=phone_number)).order_by('created_at')
+        emails = [contact.email for contact in contacts]
+        phoneNumbers = [contact.phone_number for contact in contacts]
+        secondaryContactIds = [contact.id for contact in contacts[1:]]
+        data = 	{
+		"contact":{
+			"primaryContatctId": contacts[0].id,
+			"emails": emails,
+			"phoneNumbers": phoneNumbers,
+			"secondaryContactIds": secondaryContactIds
+		}
+	}
+        return Response(data)
