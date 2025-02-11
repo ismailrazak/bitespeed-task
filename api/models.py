cc68f80ea42d7b4103084bb19baf1e68135002ca
ismail <ismailrazak009@gmail.com>
2025-02-11 19:46:21 +0530

    Add logic for creation of contacts

--- api/models.py
+++ api/models.py
@@ -1,12 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.exceptions import ObjectDoesNotExist
-class Customer(AbstractUser):
-    pass
+
 
 class Contact(models.Model):
-    customer = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="contacts")
     email = models.EmailField(blank=True,null=True)
     phone_number = models.IntegerField(blank=True,null=True)
     linked_id = models.IntegerField(blank=True,null=True)
--- api/serializers.py
+++ api/serializers.py
@@ -1,2 +1,29 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
+from django.db.models import Q
+from api.models import Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Contact
+        fields = ('customer', 'email', 'phone_number', 'linked_id', 'linked_precedence', 'created_at', 'updated_at', 'deleted_at', )
+
+
+    def create(self, validated_data):
+
+            email=validated_data.get("email")
+            phone_number =validated_data.get("phone_number")
+            contact = Contact.objects.filter(email=email,phone_number=phone_number)
+            if contact:
+                return contact
+            primary_contact = Contact.objects.filter(Q(email=email),Q(phone_number=phone_number),linked_precedence="primary")
+            if primary_contact:
+                secondary_contact=Contact.objects.create(linked_id = primary_contact.id,linked_precedence="secondary",**validated_data)
+                return secondary_contact
+            new_primary_contact=Contact.objects.create(linked_precedence='primary',**validated_data)
+            return new_primary_contact
+
 
